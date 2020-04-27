d65cffc40f15fb7e8067285ed657100c047ba35b
Tomas Trnka <modetrny@gmail.com>
2020-04-27 15:01:38 +0200

    Split Name into first and last name

--- VS_GitHub_test/Person.cs
+++ VS_GitHub_test/Person.cs
@@ -8,7 +8,8 @@ namespace VS_GitHub_test
 {
     class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
