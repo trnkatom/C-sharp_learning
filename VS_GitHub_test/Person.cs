7a8ef19cce18962c034579ef78b650327a5334b7
Tomas Trnka <modetrny@gmail.com>
2020-04-27 14:58:53 +0200

    Add new class

---
+++ VS_GitHub_test/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_GitHub_test
+{
+    class Person
+    {
+    }
+}
